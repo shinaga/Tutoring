fa62140f0c51c2dfae110026b214c2b88b512538
shinaga <shinage2@naver.com>
2022-12-01 10:38:51 +0900

    Revert "마지막 커밋"
    This reverts commit 99ff25dac7636755929508cfd4223cc5a68bf575.

--- app/src/main/java/com/example/tutoring/MainActivity.java
+++ app/src/main/java/com/example/tutoring/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //마지막 커밋
+        //아무 커밋
+
     }
 }
