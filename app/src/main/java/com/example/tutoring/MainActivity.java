99ff25dac7636755929508cfd4223cc5a68bf575
shinaga <shinage2@naver.com>
2022-12-01 10:36:02 +0900

    마지막 커밋

--- app/src/main/java/com/example/tutoring/MainActivity.java
+++ app/src/main/java/com/example/tutoring/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //아무 커밋
-
+        //마지막 커밋
     }
 }
